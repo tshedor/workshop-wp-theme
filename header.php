e4650e3a50302c6ba10258de4cdb6614e3bb6997
Tim Shedor <tshedor@gmail.com>
2012-09-18 18:32:47 -0500

    Add language-markup to html so it is inherited by prism for <code> tags

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" class="language-markup">
 <head profile="http://gmpg.org/xfn/11">
 <!--[if lt IE 7]>      <html class="ie6"> <![endif]-->
 <!--[if IE 7]>         <html class="ie7"> <![endif]-->
