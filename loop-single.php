f8d531aaef3e9ed625ef62c048812318da8ac747
Tim Shedor <tshedor@gmail.com>
2012-09-18 00:29:02 -0500

    Gets rid of the std image on single posts; fresh screenshot

--- loop-single.php
+++ loop-single.php
@@ -1,5 +1,4 @@
 <article class="post" id="<?php the_ID(); ?>">
-				<?php get_the_image(array('size' => 'medium', 'image_class' => 'pull-left')); ?>
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><h1><?php the_title(); ?></h1></a>
 				<div class="entry">
 				<?php the_content(); ?>
