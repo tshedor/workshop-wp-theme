a776f58434e95b77e684ee7bc17ece9f58fe42c9
Tim Shedor <tshedor@gmail.com>
2012-10-16 00:30:14 -0500

    retrieve only announcements on home page

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <?php get_header();
 
-	query_posts('posts_per_page=10'); while (have_posts()) : the_post();
+	query_posts('cat=5'); while (have_posts()) : the_post();
 		get_template_part( 'loop', 'single' );
 	endwhile; wp_reset_query();
 
